81bc1f4ff0fb2251e5bc20085acaa150e7e74eb0
Surajv <vsuraj2405@gmail.com>
2021-02-20 23:03:40 +0530

    created server

---
+++ Basics/firstapp.js
@@ -0,0 +1,22 @@
+//  to import files in node
+
+const http = require("http"); // to import global module , for local file -> "./..."
+
+// // method 1
+// function fun(req,res){
+// }
+// http.createServer(func);
+
+// // method 2
+// http.createServer(function(req,res)=>{
+//     //  function takes two parameters
+// })
+
+// method 3
+const server = http.createServer((req, res) => {
+  //  function takes two parameters
+  console.log(req);
+});
+//  we store it in const as server is created only once
+
+server.listen(3000);
