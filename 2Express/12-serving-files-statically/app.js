e9dde68f1b22fb9062fbedd971132279e0cea929
Surajv <vsuraj2405@gmail.com>
2021-02-23 12:36:21 +0530

    added important notes...

--- 2Express/12-serving-files-statically/app.js
+++ 2Express/12-serving-files-statically/app.js
@@ -10,6 +10,7 @@ const shopRoutes = require("./routes/shop");
 const { static } = require("express");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+// serving static files in the public folder... it has all css files...
 app.use(express.static(path.join(__dirname, "public")));
 
 // Here express.static is added because when it pulls the html document, we know that
@@ -17,6 +18,10 @@ app.use(express.static(path.join(__dirname, "public")));
 // otherwise only the html file would be rendered via app.js. So to render
 // all the other static files with our sent file(in res for our req.), we use static.
 /*
+NOTE: In our html files we have removed the: /public/css... path 
+as express is serving the static files from the public folder we have passed
+in as parameter.: app.use(express.static(path.join(__dirname, "public")));
+
 [OR]
 You are not limited to serving dummy text files. 
 you can sendFile() to your users, if req. directly made for a file (eg. .css)
